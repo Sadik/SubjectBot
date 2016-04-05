9c0ac1f5c71fb53635b05e647613f2771de8bc5f
Sadik Özoguz <sadik@informatik.uni-bremen.de>
2016-04-05 17:53:04 +0200

    indent

--- subjectBot.py
+++ subjectBot.py
@@ -78,34 +78,35 @@ def listener(messages):
             tb.reply_to(m, "Only text messages are supported")
             return
 
-    if answerText is not None: #command was executed, send reply
-        tb.send_message(m.chat.id, answerText)
-    else:
-        # start Filtering message for content
-        mFilter = MessageFilter.MessageFilter(m)
-        mFilter.NER()
+        if answerText is not None: #command was executed, send reply
+            tb.send_message(m.chat.id, answerText)
+        else:
+            # start Filtering message for content
+            mFilter = MessageFilter.MessageFilter(m)
+            pos_list = mFilter.NER()
+            #mFilter.NER()
 
-        relevance = mFilter.isProbablyRelevant()
-        if (relevance == 2):
-            #tb.send_message(m.chat.id, "Deine Nachricht ist wahrscheinlich relevant.")
-            print ("Deine Nachricht ist wahrscheinlich relevant.")
-            #tb.send_message(m.chat.id, mFilter.showFlags())
-            result = mFilter.updateOrCreateEventFrame(m)
-            tb.send_message(m.chat.id, result)
-        elif (relevance == 1):
-            if (mFilter.isContextRelevant()):
-                #tb.send_message(m.chat.id, "Deine Nachricht ist im Kontext relevant.")
-                print ("Deine Nachricht ist im Kontext relevant.")
+            relevance = mFilter.isProbablyRelevant2(pos_list)
+            if (relevance == 2):
+                #tb.send_message(m.chat.id, "Deine Nachricht ist wahrscheinlich relevant.")
+                print ("Deine Nachricht ist wahrscheinlich relevant.")
                 #tb.send_message(m.chat.id, mFilter.showFlags())
                 result = mFilter.updateOrCreateEventFrame(m)
                 tb.send_message(m.chat.id, result)
-        else:
-            #tb.send_message(m.chat.id, "irrelevant")
-            print ("irrelevant")
+            elif (relevance == 1):
+                if (mFilter.isContextRelevant()):
+                    #tb.send_message(m.chat.id, "Deine Nachricht ist im Kontext relevant.")
+                    print ("Deine Nachricht ist im Kontext relevant.")
+                    #tb.send_message(m.chat.id, mFilter.showFlags())
+                    result = mFilter.updateOrCreateEventFrame(m)
+                    tb.send_message(m.chat.id, result)
+            else:
+                #tb.send_message(m.chat.id, "irrelevant")
+                print ("irrelevant")
 
-        #users_messages = Helper.get_users_latest_messages(m.chat.id, m.from_user.id)
-        #print ("users messages: " + str(len(users_messages)))
-        #print (users_messages)
+            #users_messages = Helper.get_users_latest_messages(m.chat.id, m.from_user.id)
+            #print ("users messages: " + str(len(users_messages)))
+            #print (users_messages)
 
 
 def greetings(chatid):
